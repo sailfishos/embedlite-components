da8edb9cec21b2b8e2dc89bc9b2e997a2e4b6847
Raine Makelainen <raine.makelainen@jolla.com>
2016-06-29 11:57:46 +0300

    [embedlite-components] Drop "embed:domcontentloaded" message. Contributes to JB#35379
    Use "chrome:contentloaded" in case needed.

--- jsscripts/embedhelper.js
+++ jsscripts/embedhelper.js
@@ -536,7 +536,6 @@ EmbedHelper.prototype = {
         if (LoginManagerContent.onContentLoaded) {
           LoginManagerContent.onContentLoaded(aEvent);
         }
-        this._handleDomContentLoaded(aEvent);
         break;
       }
       case "DOMFormHasPassword": {
@@ -570,16 +569,6 @@ EmbedHelper.prototype = {
       return false;
     }
     return this.contentDocumentIsDisplayed;
-  },
-
-  _handleDomContentLoaded: function(aEvent) {
-    let window = aEvent.target.defaultView;
-    if (window) {
-      let winid = Services.embedlite.getIDByWindow(window);
-      try {
-        Services.embedlite.sendAsyncMessage(winid, "embed:domcontentloaded", JSON.stringify({ "rootFrame": window.parent === window }));
-      } catch (e) {}
-    }
   },
 
   _handleFullScreenChanged: function(aEvent) {
