2414869100ee1e6c0afa1208a85fee2c2ae401d6
Raine Makelainen <raine.makelainen@jolla.com>
2021-08-31 14:51:56 +0300

    [embedlite-components] Fix addEventListener reference errors from ContextMenu. JB#55336 OMP#JOLLA-331

--- jsscripts/embedhelper.js
+++ jsscripts/embedhelper.js
@@ -156,7 +156,7 @@ EmbedHelper.prototype = {
         let element = this._touchElement;
         if (element) {
           let [x, y] = [aMessage.json.x, aMessage.json.y];
-          ContextMenuHandler._processPopupNode(element, x, y, Ci.nsIDOMMouseEvent.MOZ_SOURCE_UNKNOWN);
+          gContextMenuHandler._processPopupNode(element, x, y, Ci.nsIDOMMouseEvent.MOZ_SOURCE_UNKNOWN);
         }
         this._touchElement = null;
         break;
@@ -596,6 +596,7 @@ this.ContentScroll = ContentScroll;
 Services.scriptloader.loadSubScript("chrome://embedlite/content/Logger.js");
 Services.scriptloader.loadSubScript("chrome://embedlite/content/Util.js");
 Services.scriptloader.loadSubScript("chrome://embedlite/content/ContextMenuHandler.js");
+gContextMenuHandler.init(content);
 Services.scriptloader.loadSubScript("chrome://embedlite/content/SelectionPrototype.js");
 Services.scriptloader.loadSubScript("chrome://embedlite/content/SelectionHandler.js");
 Services.scriptloader.loadSubScript("chrome://embedlite/content/SelectAsyncHelper.js");
